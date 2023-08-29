3d916b7b242b4f58aa3824418b7b7450b64dd964
samuelbernal44 <samuelbernal44@gmail.com>
2023-08-29 18:25:04 -0500

    Cambios ESLint

--- src/models/Booking.js
+++ src/models/Booking.js
@@ -3,37 +3,37 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Booking', {
-
-   id: {
-    type: DataTypes.UUID,
-    primaryKey: true,
-    defaultValue: DataTypes.UUIDV4,
-    allowNull: false
-   }, 
-   startDate: {
-    type: DataTypes.DATE,
-    allowNull: false,
-   },
-   finishDate: {
-    type: DataTypes.DATE,
-    allowNull: false,
-   },
-   pickUpLocationId: {
-    type: DataTypes.UUID,
-    allowNull: false,
-   },
-   returnLocationId: {
-    type: DataTypes.UUID,
-    allowNull: false,
-   }
-  }, {
-    timestamps: false
-  }
-);
-}
-// 
-//Booking.belongsTo(Location, { as: 'pickUpLocation', foreignKey: 'pickUpLocationId' });
-//Booking.belongsTo(Location, { as: 'returnLocation', foreignKey: 'returnLocationId' });
-
-
+  sequelize.define(
+    'Booking',
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
+      startDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      finishDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      pickUpLocationId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      returnLocationId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    },
+  );
+};
+//
+// Booking.belongsTo(Location, { as: 'pickUpLocation', foreignKey: 'pickUpLocationId' });
+// Booking.belongsTo(Location, { as: 'returnLocation', foreignKey: 'returnLocationId' });
--- src/models/Location.js
+++ src/models/Location.js
@@ -3,34 +3,34 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Location', {
-
-   id: {
-    type: DataTypes.UUID,
-    primaryKey: true,
-    defaultValue: DataTypes.UUIDV4,
-    allowNull: false
-   }, 
-   address: {
-    type: DataTypes.STRING,
-    allowNull: false,
-   },
-   city: {
-    type: DataTypes.STRING,
-    allowNull: false,
-   },
-   country: {
-    type: DataTypes.STRING,
-    allowNull: false,
-   },
-   zipCode: {
-    type: DataTypes.STRING,
-    allowNull: false,
-   }
-  }, {
-    timestamps: false
-  }
-);
-}
-
-
+  sequelize.define(
+    'Location',
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      city: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      country: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      zipCode: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    },
+  );
+};
