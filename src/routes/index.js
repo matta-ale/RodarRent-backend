34bf2d47e0fdc5ac1f1eb1227814bc1822a968b6
Alexis Mattaruco <matta.ale@gmail.com>
2023-08-30 16:46:34 -0300

    agregadas rutas para getCustomerById y deleteCustomerById

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,13 @@ const { Router } = require('express');
 const postValidate = require('../middlewares/postValidate');
 const createCustomer = require('../controllers/createCustomer');
 const getAllCustomers = require('../controllers/getAllCustomers');
+const getCustomerById = require('../controllers/getCustomerById');
+const deleteCustomerById = require('../controllers/deleteCustomerById');
 const { createPay } = require('../controllers/createPay');
 const { getAllPayments } = require('../controllers/getAllPayments');
+const createCustomerValidation = require('../middlewares/createCustomerValidation');
+const getCustomerByIdValidation = require('../middlewares/getCustomerByIdValidation');
+const deleteCustomerByIdValidation = require('../middlewares/deleteCustomerByIdValidation');
 
 const router = Router();
 
@@ -13,8 +18,10 @@ router.get('/hc', (req, res) => {
   res.status(200).send('Server up');
 });
 
-router.post('/customers', createCustomer);
+router.post('/customers',createCustomerValidation, createCustomer);
 router.get('/customers', getAllCustomers);
+router.get('/customers/:id',getCustomerByIdValidation, getCustomerById);
+router.delete('/customers/:id',deleteCustomerByIdValidation, deleteCustomerById);
 router.post('/payments', postValidate, createPay);
 router.get('/payments', getAllPayments);
 
