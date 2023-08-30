a492f7191f68fa88e9279ad337c93f03c856939c
samuelbernal44 <samuelbernal44@gmail.com>
2023-08-30 08:44:21 -0500

    Ruta para consultar todos los pagos

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,18 @@ const { Router } = require('express');
 const createCustomer = require('../controllers/createCustomer');
 const getAllCustomers = require('../controllers/getAllCustomers');
 const { createPay } = require('../controllers/createPay');
+const { getAllPayments } = require('../controllers/getAllPayments');
+
 const router = Router();
 
-router.get('/hc',(req,res) => {  //healthcheck
-    res.status(200).send('Server up')
-})
+router.get('/hc', (req, res) => {
+  // healthcheck
+  res.status(200).send('Server up');
+});
 
-router.post('/customers',createCustomer)
-router.get('/customers',getAllCustomers)
+router.post('/customers', createCustomer);
+router.get('/customers', getAllCustomers);
 router.post('/payments', createPay);
+router.get('/payments', getAllPayments);
 
 module.exports = router;
