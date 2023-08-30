728c570f50f55687f312126fb920958cf8ae2c94
samuelbernal44 <samuelbernal44@gmail.com>
2023-08-30 09:26:46 -0500

    Validación de post para createPay

---
+++ src/middlewares/postValidate.js
@@ -0,0 +1,14 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable consistent-return */
+const postValidate = (req, res, next) => {
+  const { amount, date, method, status } = req.body;
+
+  if (!amount) return res.status(404).json({ error: 'Missing amount' });
+  if (!date) return res.status(404).json({ error: 'Missing date' });
+  if (!method) return res.status(404).json({ error: 'Missing pay method' });
+  if (!status) return res.status(404).json({ error: 'Missing status' });
+
+  next();
+};
+
+module.exports = postValidate;
