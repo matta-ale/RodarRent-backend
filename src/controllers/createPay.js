0066f731a6cb4d132cf3a5fe5a7ca910e401cbfb
samuelbernal44 <samuelbernal44@gmail.com>
2023-08-30 08:28:47 -0500

    Eliminar console.log y Corrección nombre del handler

--- src/controllers/createPay.js
+++ src/controllers/createPay.js
@@ -1,9 +1,8 @@
-const { handleCreatePay } = require('../handlers/handleCreatePay');
+const { createPayHandler } = require('../handlers/createPayHandler');
 
 async function createPay(req, res) {
-  console.log(req.body);
   try {
-    const payment = await handleCreatePay(req.body);
+    const payment = await createPayHandler(req.body);
     res.status(201).json(payment);
   } catch (error) {
     res.status(400).json({ error: error.message });
