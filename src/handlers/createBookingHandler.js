d6f3e2315226fd1edb2caf9d688b55653cca77ac
Matiaso19 <matiasscrog@gmail.com>
2023-08-31 19:28:37 -0300

    arreglado createBooking y modelo

--- src/handlers/createBookingHandler.js
+++ src/handlers/createBookingHandler.js
@@ -1,12 +1,12 @@
 const { Booking } = require('../db');
 
-const createBookingHandler = async ({startDate,finishDate,pickUpLocationId, returnLocationId, stateBooking}) => {
+const createBookingHandler = async ({startDate,finishDate,pickUpLocationId, returnLocationId}) => {
   const booking = await Booking.create({
     startDate,
     finishDate,
     pickUpLocationId,
     returnLocationId,
-    stateBooking
+    
   });
   return booking;
 }
