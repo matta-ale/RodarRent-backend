5fdc535b3ccd51db106040ecd426271657a6cf90
samuelbernal44 <samuelbernal44@gmail.com>
2023-09-01 13:24:05 -0500

    Controlador actualización de pago

---
+++ src/controllers/pay/updatePayment.js
@@ -0,0 +1,14 @@
+const {
+  updatePaymentHandler,
+} = require('../../handlers/pay/updatePaymentHandler');
+
+async function updatePayment(req, res) {
+  const result = await updatePaymentHandler(req.params.id, req.body);
+  if (result.error) {
+    res.status(result.status).json({ error: result.error });
+  } else {
+    res.json(result.payment);
+  }
+}
+
+module.exports = { updatePayment };
