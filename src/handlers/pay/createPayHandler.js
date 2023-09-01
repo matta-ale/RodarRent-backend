aedfe28ea80f5e987ca76491fe12b40214143ae5
samuelbernal44 <samuelbernal44@gmail.com>
2023-08-31 20:32:15 -0500

    Vincular pago a una reserva

--- src/handlers/pay/createPayHandler.js
+++ src/handlers/pay/createPayHandler.js
@@ -1,9 +1,11 @@
 /* eslint-disable object-curly-newline */
-const { Pay } = require('../../db');
+const { Pay, Booking } = require('../../db');
 const CustomError = require('../../utils/customError');
 
-async function createPayHandler({ amount, date, method, status }) {
+async function createPayHandler({ id, amount, date, method, status }) {
   try {
+    // eslint-disable-next-line object-shorthand
+    const booking = await Booking.findOne({ where: { id: id } });
     const payment = await Pay.create({
       amount,
       date,
@@ -13,6 +15,7 @@ async function createPayHandler({ amount, date, method, status }) {
     if (!payment) {
       throw new CustomError('Failed to create payment', 500);
     }
+    await booking.setPay(payment);
     return payment;
   } catch (error) {
     throw new CustomError(error.message, 500);
