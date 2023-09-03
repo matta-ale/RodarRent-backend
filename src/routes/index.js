79bd510fb3a6d0225f14758bce70c9c0fa433618
Matiaso19 <matiasscrog@gmail.com>
2023-09-03 12:16:11 -0300

    ruta delete Booking

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,58 +1,63 @@
-const { Router } = require('express');
+const { Router } = require("express");
 
+const createCustomer = require("../controllers/customers/createCustomer");
+const bulkCreateCustomers = require("../controllers/customers/bulkCreateCustomers");
+const getAllCustomers = require("../controllers/customers/getAllCustomers");
+const getFilteredCustomers = require("../controllers/customers/getFilteredCustomers");
+const updateCustomer = require("../controllers/customers/updateCustomer");
+const getCustomerById = require("../controllers/customers/getCustomerById");
+const deleteCustomerById = require("../controllers/customers/deleteCustomerById");
+const createCustomerValidation = require("../middlewares/createCustomerValidation");
+const getCustomerByIdValidation = require("../middlewares/getCustomerByIdValidation");
+const deleteCustomerByIdValidation = require("../middlewares/deleteCustomerByIdValidation");
+const createVehicles = require("../controllers/vehicles/createVehicles");
+const postVehiclesValidate = require("../middlewares/postVehiclesValidate");
+const postBookingValidate = require("../middlewares/postBookingValidate");
+const createBooking = require("../controllers/bookings/createBooking");
+const { getAllBookings } = require("../controllers/bookings/getAllBookings");
+const getVehicleById = require("../controllers/vehicles/getVehicleById");
+const getAllVehicles = require("../controllers/vehicles/getAllVehicles");
+const { getAllLocations } = require("../controllers/locations/getAllLocations");
+const { createLocation } = require("../controllers/locations/createLocation");
+const updateVehicle = require("../controllers/vehicles/updateVehicle");
+const getAllAvailable = require("../controllers/vehicles/getAllAvailable");
+const { getLocationById } = require("../controllers/locations/getLocationById");
+const { getBookingById } = require("../controllers/bookings/getBookingById");
+const { updateBooking } = require("../controllers/bookings/updateBooking");
+const { updateLocation } = require("../controllers/locations/updateLocation");
+const {
+  deleteBookingValidation,
+} = require("../middlewares/deleteBookingValidation");
+const { deleteBooking } = require("../controllers/bookings/deleteBooking");
 
-const createCustomer = require('../controllers/customers/createCustomer');
-const bulkCreateCustomers = require('../controllers/customers/bulkCreateCustomers');
-const getAllCustomers = require('../controllers/customers/getAllCustomers');
-const getFilteredCustomers = require('../controllers/customers/getFilteredCustomers');
-const updateCustomer = require('../controllers/customers/updateCustomer');
-const getCustomerById = require('../controllers/customers/getCustomerById');
-const deleteCustomerById = require('../controllers/customers/deleteCustomerById');
-const createCustomerValidation = require('../middlewares/createCustomerValidation');
-const getCustomerByIdValidation = require('../middlewares/getCustomerByIdValidation');
-const deleteCustomerByIdValidation = require('../middlewares/deleteCustomerByIdValidation');
-const createVehicles = require('../controllers/vehicles/createVehicles');
-const postVehiclesValidate = require('../middlewares/postVehiclesValidate');
-const postBookingValidate = require('../middlewares/postBookingValidate');
-const createBooking = require('../controllers/bookings/createBooking');
-const { getAllBookings } = require('../controllers/bookings/getAllBookings');
-const getVehicleById = require('../controllers/vehicles/getVehicleById');
-const getAllVehicles = require('../controllers/vehicles/getAllVehicles');
-const { getAllLocations } = require('../controllers/locations/getAllLocations');
-const { createLocation } = require('../controllers/locations/createLocation');
-const updateVehicle = require('../controllers/vehicles/updateVehicle');
-const getAllAvailable = require('../controllers/vehicles/getAllAvailable');
-const { getLocationById } = require('../controllers/locations/getLocationById');
-const { getBookingById } = require('../controllers/bookings/getBookingById');
-const { updateBooking } = require('../controllers/bookings/updateBooking');
 const router = Router();
-router.get('/hc', (req, res) => {
+router.get("/hc", (req, res) => {
   // healthcheck
-  res.status(200).send('Server up');
+  res.status(200).send("Server up");
 });
-router.post('/vehicles', postVehiclesValidate, createVehicles);
-router.get('/vehicles/:domain', getVehicleById);
-router.get('/vehicles', getAllVehicles);
-router.put('/vehicles', updateVehicle);
-router.post('/customers/bulk', bulkCreateCustomers);
-router.post('/customers', createCustomerValidation, createCustomer);
-router.get('/customers', getAllCustomers);
-router.get('/customers/filter', getFilteredCustomers);
-router.get('/customers/:id', getCustomerByIdValidation, getCustomerById);
-router.put('/customers', createCustomerValidation, updateCustomer);
+router.post("/vehicles", postVehiclesValidate, createVehicles);
+router.get("/vehicles/:domain", getVehicleById);
+router.get("/vehicles", getAllVehicles);
+router.put("/vehicles", updateVehicle);
+router.post("/customers/bulk", bulkCreateCustomers);
+router.post("/customers", createCustomerValidation, createCustomer);
+router.get("/customers", getAllCustomers);
+router.get("/customers/filter", getFilteredCustomers);
+router.get("/customers/:id", getCustomerByIdValidation, getCustomerById);
+router.put("/customers", createCustomerValidation, updateCustomer);
 router.delete(
-  '/customers/:id',
+  "/customers/:id",
   deleteCustomerByIdValidation,
-  deleteCustomerById,
+  deleteCustomerById
 );
-router.get('/bookings', getAllBookings);
-router.post('/bookings', postBookingValidate, createBooking);
-router.get('/bookings/:id', getBookingById);
-router.get('/locations', getAllLocations);
-router.post('/locations', createLocation);
-router.get('/locations/:id', getLocationById);
-router.get('/available', getAllAvailable);
-router.put('/bookings/:id', updateBooking);
-
-
+router.get("/bookings", getAllBookings);
+router.post("/bookings", postBookingValidate, createBooking);
+router.get("/bookings/:id", getBookingById);
+router.get("/locations", getAllLocations);
+router.post("/locations", createLocation);
+router.get("/locations/:id", getLocationById);
+router.get("/available", getAllAvailable);
+router.put("/bookings/:id", updateBooking);
+router.put("/locations/:id", updateLocation);
+router.delete("/bookings/:id", deleteBookingValidation, deleteBooking);
 module.exports = router;
