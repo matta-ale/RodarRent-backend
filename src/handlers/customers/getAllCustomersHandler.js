751371248c1dbd67394c60e6e795d520a4f4135c
matta-ale <matta.ale@gmail.com>
2023-09-03 17:49:56 -0300

    paginado agregado en getAllCustomers y getFilteredCustomers

--- src/handlers/customers/getAllCustomersHandler.js
+++ src/handlers/customers/getAllCustomersHandler.js
@@ -1,10 +1,26 @@
 const { Customer } = require('../../db');
 
-const getAllCustomersHandler = async (data) => {
+const getAllCustomersHandler = async (page,pageSize) => {
   try {
-    const customers = await Customer.findAll({where: {isActive:true}});
-    if(!customers) throw new Error('No customers in database')
-    return customers
+    const offset = (page-1)*pageSize
+    const customers = await Customer.findAndCountAll({
+      where: {isActive:true},
+      offset,
+      limit: pageSize,
+    });
+    if(customers.rows.length===0) throw new Error('No customers in database')
+    const totalPages = Math.ceil(customers.count / pageSize);
+
+    const pagination = {
+      currentPage: page,
+      pageSize: pageSize,
+      totalItems: customers.count,
+      totalPages: totalPages,
+      nextPage: page < totalPages ? page + 1 : null,
+      prevPage: page > 1 ? page - 1 : null,
+    };
+
+    return { data: customers.rows, pagination };
   } catch (error) {
     throw error;
   }
