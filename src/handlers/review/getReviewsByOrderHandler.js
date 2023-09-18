3345c8452f3678ca6d45f7ce9e00f88a118e2c06
samuelbernal44 <samuelbernal44@gmail.com>
2023-09-18 18:53:53 -0500

    Se mejora el filtro de ordenamiento

--- src/handlers/review/getReviewsByOrderHandler.js
+++ src/handlers/review/getReviewsByOrderHandler.js
@@ -1,24 +1,47 @@
-const { Review } = require('../../db');
+const { Op } = require('sequelize');
+const { Review, Customer } = require('../../db');
 
 async function getReviewsByOrderHandler(order) {
   let reviews;
   if (order === 'desc') {
     reviews = await Review.findAll({
+      include: {
+        model: Customer,
+        attributes: ['name'],
+      },
+      where: {
+        rating: {
+          [Op.or]: [4, 5], // Filtra las revisiones con rating 4 y 5
+        },
+      },
       order: [
         ['rating', 'DESC'],
-        ['createdAt', 'DESC'], // add this line to sort by timestamp
+        ['createdAt', 'DESC'],
       ],
     });
   } else if (order === 'asc') {
     reviews = await Review.findAll({
+      include: {
+        model: Customer,
+        attributes: ['name'],
+      },
+      where: {
+        rating: {
+          [Op.or]: [1, 2], // Filtra las revisiones con rating 1 y 2
+        },
+      },
       order: [
         ['rating', 'ASC'],
-        ['createdAt', 'DESC'], // add this line to sort by timestamp
+        ['createdAt', 'DESC'],
       ],
     });
   } else if (order === 'date') {
     reviews = await Review.findAll({
-      order: [['createdAt', 'DESC']], // sort by timestamp only
+      include: {
+        model: Customer,
+        attributes: ['name'],
+      },
+      order: [['createdAt', 'DESC']],
     });
   }
 
