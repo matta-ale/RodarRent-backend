6b38a0b4f84667ba19eba5cac40e95e78a187820
samuelbernal44 <samuelbernal44@gmail.com>
2023-09-24 15:46:40 -0500

    Establecer un intervalo de tiempo mails de reviews

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 //acá conecto sequelize con el server
-const server = require("./src/app.js");
-const { conn } = require("./src/db.js");
-const performHealthCheck = require("./src/utils/performHealthCheck.js");
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+const performHealthCheck = require('./src/utils/performHealthCheck.js');
 const sendEmailsToCustomers = require('./src/utils/sendEmailsToCustomers');
 
 // Syncing all the models at once.
 conn.sync({ force: false, alter: false }).then(async () => {
   server.listen(3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+    console.log('%s listening at 3001'); // eslint-disable-line no-console
     performHealthCheck();
     sendEmailsToCustomers();
     setInterval(performHealthCheck, 60000);
   });
+  setInterval(sendEmailsToCustomers, 24 * 60 * 60 * 1000);
 });
 
 //sync es una promesa, en el then levanto el server
