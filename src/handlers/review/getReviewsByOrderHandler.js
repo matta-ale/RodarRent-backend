ff3feb5c641433437acf8a616f7506af41abf62d
samuelbernal44 <samuelbernal44@gmail.com>
2023-09-25 17:29:42 -0500

    Se adiciona al ordenamiento de rating el de fecha

--- src/handlers/review/getReviewsByOrderHandler.js
+++ src/handlers/review/getReviewsByOrderHandler.js
@@ -3,46 +3,36 @@ const { Review, Customer } = require('../../db');
 
 async function getReviewsByOrderHandler(order) {
   let reviews;
+  const commonOptions = {
+    include: {
+      model: Customer,
+      attributes: ['name'],
+    },
+    order: [['createdAt', 'DESC']], // Sort by date in descending order
+  };
+
   if (order === 'desc') {
     reviews = await Review.findAll({
-      include: {
-        model: Customer,
-        attributes: ['name'],
-      },
+      ...commonOptions,
       where: {
         rating: {
-          [Op.or]: [4, 5], // Filtra las revisiones con rating 4 y 5
+          [Op.or]: [4, 5], // Filter reviews with rating 4 and 5
         },
       },
-      order: [
-        ['rating', 'DESC'],
-        ['createdAt', 'DESC'],
-      ],
+      order: [['rating', 'DESC'], ...commonOptions.order], // Add rating sorting
     });
   } else if (order === 'asc') {
     reviews = await Review.findAll({
-      include: {
-        model: Customer,
-        attributes: ['name'],
-      },
+      ...commonOptions,
       where: {
         rating: {
-          [Op.or]: [1, 2], // Filtra las revisiones con rating 1 y 2
+          [Op.or]: [1, 2], // Filter reviews with rating 1 and 2
         },
       },
-      order: [
-        ['rating', 'ASC'],
-        ['createdAt', 'DESC'],
-      ],
+      order: [['rating', 'ASC'], ...commonOptions.order], // Add rating sorting
     });
   } else if (order === 'date') {
-    reviews = await Review.findAll({
-      include: {
-        model: Customer,
-        attributes: ['name'],
-      },
-      order: [['createdAt', 'DESC']],
-    });
+    reviews = await Review.findAll(commonOptions);
   }
 
   return reviews;
